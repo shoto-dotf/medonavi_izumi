b1b7d46186995d08a2a3f8a5f49c16d0ea869335
izumi244 <izumiyama_t@dot-f.jp>
2025-06-12 22:38:25 +0900

    Delete project directory

--- project/src/pages/ChatPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ChatArea from '../components/ChatArea';
-
-const ChatPage: React.FC = () => {
-  return <ChatArea />;
-};
-
-export default ChatPage;
--- project/src/utils/markdown.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-
-export const parseMarkdown = (text: string): React.ReactElement | null => {
-  if (!text) return null;
-  
-  return <ReactMarkdown>{text}</ReactMarkdown>;
-};
